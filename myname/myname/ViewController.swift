36e0adbd04ab97b28f6fbc2fd8a18004397b5a77
ICMMAC08-5617 <icmmac08-5617@ICMMAC08-5617s-Mac-mini.local>
2022-08-10 09:28:02 -0400

    app Myname

---
+++ myname/myname/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myname
+//
+//  Created by ICMMAC08-5617 on 02/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
